28dce781a024014f754a7d59b4a0e953b2ac8890
Darin Minamoto <darinm223@gmail.com>
2015-09-03 21:09:57 -0700

    Added UI for Add Note view controller

--- location-notes/DMAddNoteViewController.m
+++ location-notes/DMAddNoteViewController.m
@@ -9,7 +9,11 @@
 #import <Parse/Parse.h>
 #import "DMAddNoteViewController.h"
 
-@interface DMAddNoteViewController ()
+@interface DMAddNoteViewController () {
+    NSString *noNoteText;
+}
+
+@property (nonatomic, weak) IBOutlet UITextView *noteText;
 
 @end
 
@@ -18,7 +22,15 @@
 - (instancetype)initWithNote:(PFObject *)note {
     self = [super init];
     if (self) {
-        self.note = note;
+        if (note == nil) {
+            PFObject *newNote = [[PFObject alloc] initWithClassName:@"Note"];
+            [newNote setObject:[PFUser currentUser] forKey:@"user"];
+            self.note = newNote;
+        } else {
+            self.note = note;
+        }
+        
+        noNoteText = @"Enter note here...";
     }
     return self;
 }
@@ -27,11 +39,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    // Set gesture recogizers
+    UITapGestureRecognizer *tapInTextField = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTapInTextField:)];
+    tapInTextField.numberOfTapsRequired = 1;
+    tapInTextField.cancelsTouchesInView = NO;
+    [self.noteText addGestureRecognizer:tapInTextField];
+    
+    UITapGestureRecognizer *tapOutTextField = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTapOutsideTextField:)];
+    tapOutTextField.numberOfTapsRequired = 1;
+    tapOutTextField.cancelsTouchesInView = NO;
+    [self.view addGestureRecognizer:tapOutTextField];
+    
     UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelNote:)];
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(saveNote:)];
     
     self.navigationItem.leftBarButtonItem = cancelButton;
     self.navigationItem.rightBarButtonItem = doneButton;
+    
+    // Set note properties
+    NSString *noteStr = [self.note objectForKey:@"note"];
+    if (noteStr == nil || noteStr.length == 0) {
+        self.noteText.text = noNoteText;
+    } else {
+        self.noteText.text = noteStr;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,20 +72,31 @@
 
 #pragma mark - Actions
 
+- (IBAction)onTapInTextField:(id)sender {
+    if ([self.noteText.text isEqualToString:noNoteText]) {
+        self.noteText.text = @"";
+    }
+    [self.noteText becomeFirstResponder];
+}
+                                                                                                        
+- (IBAction)onTapOutsideTextField:(id)sender {
+    if (self.noteText.text.length == 0) {
+        self.noteText.text = noNoteText;
+    }
+    [self.noteText resignFirstResponder];
+}
+
 - (IBAction)cancelNote:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (IBAction)saveNote:(id)sender {
-    // TODO(darin): implement saving of note
-    if (self.note == nil) {
-        // Create and save new note
-        PFObject *newNote = [[PFObject alloc] initWithClassName:@"Note"];
-        [newNote setObject:[PFUser currentUser] forKey:@"user"];
-        [newNote setObject:@"Hello" forKey:@"note"];
-        [newNote saveInBackgroundWithTarget:self selector:@selector(onSavedNote:)];
+    NSLog(@"Note text: %@", self.noteText.text);
+    if ([self.noteText.text isEqualToString:noNoteText] || self.noteText.text.length == 0) {
+        UIAlertView *noNoteAlert = [[UIAlertView alloc] initWithTitle:@"Note text required" message:@"You need to fill in the note description" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+        [noNoteAlert show];
     } else {
-        // Save existing note
+        [self.note setObject:self.noteText.text forKey:@"note"];
         [self.note saveInBackgroundWithTarget:self selector:@selector(onSavedNote:)];
     }
 }
