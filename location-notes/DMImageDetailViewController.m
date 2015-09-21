1e48944a692a06b1bd726265bdde47103fffc737
Darin Minamoto <darinm223@gmail.com>
2015-09-20 18:32:09 -0700

    Started on image email sending

--- location-notes/DMImageDetailViewController.m
+++ location-notes/DMImageDetailViewController.m
@@ -6,10 +6,11 @@
 //  Copyright (c) 2015 com.d_m. All rights reserved.
 //
 
+#import <MessageUI/MFMailComposeViewController.h>
 #import "DMImageDetailViewController.h"
 #import "DMImageStore.h"
 
-@interface DMImageDetailViewController ()
+@interface DMImageDetailViewController () <UIGestureRecognizerDelegate, MFMailComposeViewControllerDelegate>
 
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
 
@@ -34,10 +35,19 @@ static const double ANIMATION_DURATION = 0.5;
     
     self.imageView.userInteractionEnabled = YES;
     
+    // Add touch recognizers
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(imageTapped:)];
+    tapRecognizer.numberOfTapsRequired = 1;
+    tapRecognizer.delaysTouchesBegan = YES;
+    tapRecognizer.delegate = self;
+    [self.imageView addGestureRecognizer:tapRecognizer];
+    
     // Add swipe recognizers
     UISwipeGestureRecognizer *leftSwipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(rightArrowSelected:)];
     UISwipeGestureRecognizer *rightSwipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(leftArrowSelected:)];
     
+    leftSwipeRecognizer.delegate = self;
+    rightSwipeRecognizer.delegate = self;
     leftSwipeRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
     rightSwipeRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
     [self.imageView addGestureRecognizer:leftSwipeRecognizer];
@@ -60,7 +70,60 @@ static const double ANIMATION_DURATION = 0.5;
 }
 
 #pragma mark -
+#pragma mark UIGestureRecognizerDelegate methods
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
+
+#pragma mark -
+#pragma mark MFMailComposeViewControllerDelegate methods
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    if (result == MFMailComposeResultFailed) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Email error" message:@"Error sending email" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark -
 #pragma mark Interface Builder Actions
+                                             
+- (IBAction)imageTapped:(id)sender {
+    UIAlertController *actionSheetController = [UIAlertController alertControllerWithTitle:@"Picture options" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    
+    UIAlertAction *takePictureAction = [UIAlertAction actionWithTitle:@"Send through email" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
+        mailController.mailComposeDelegate = self;
+        [mailController setSubject:[NSString stringWithFormat:@"Sending Note #%ld", self.imageIndex]];
+        [mailController setMessageBody:@"Your image data is attached." isHTML:NO];
+        
+        // Attach image data to email
+        UIImage *currentImage = [self.imageStore imageForIndex:self.imageIndex];
+        NSData *jpegData = UIImageJPEGRepresentation(currentImage, 1);
+        [mailController addAttachmentData:jpegData mimeType:@"image/jpeg" fileName:[NSString stringWithFormat:@"image%ld", self.imageIndex]];
+        
+        if (mailController && [MFMailComposeViewController canSendMail]) {
+            [self presentViewController:mailController animated:YES completion:nil];
+        } else {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Email error" message:@"Cannot send email" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil, nil];
+            [alertView show];
+        }
+    }];
+    
+    [actionSheetController addAction:cancelAction];
+    [actionSheetController addAction:takePictureAction];
+    
+    [self presentViewController:actionSheetController animated:YES completion:nil];
+}
 
 - (IBAction)leftArrowSelected:(UIGestureRecognizer *)gestureRecognizer {
     if (self.imageIndex - 1 >= 0) {
